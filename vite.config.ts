a7be95637887d1e6c3eeca0d6e1d00c37afa861d
alexey_kozharin <alexey_kozharin@relex.ru>
2023-07-11 13:19:07 +0300

    config correction

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { VitePWA } from 'vite-plugin-pwa';
-import path from 'path';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import { VitePWA } from "vite-plugin-pwa"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       devOptions: {
-        enabled: true
-      }
-    })],
+        enabled: true,
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      external: ["@emotion/react"],
+    },
+  },
 })
-
