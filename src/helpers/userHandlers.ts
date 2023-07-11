9eec0165c9cb648ab71748b3d264e484a581e267
alexey_kozharin <alexey_kozharin@relex.ru>
2023-07-11 12:42:27 +0300

    import correction

--- src/helpers/userHandlers.ts
+++ src/helpers/userHandlers.ts
@@ -1,35 +1,35 @@
-import { Player } from '../models/messages';
+import { Player } from "../models/player-models"
 
 const shuffle = (array: Array<number>): Array<number> => {
-  const shuffledArray = [...array];
+  const shuffledArray = [...array]
   let currentIndex = shuffledArray.length,
-    randomIndex;
+    randomIndex
 
   while (currentIndex) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
 
-    [shuffledArray[currentIndex], shuffledArray[randomIndex]] = [
+    ;[shuffledArray[currentIndex], shuffledArray[randomIndex]] = [
       shuffledArray[randomIndex],
       shuffledArray[currentIndex],
-    ];
+    ]
   }
-  return shuffledArray;
-};
+  return shuffledArray
+}
 
 export const getInitialPlayer = (): Player => {
-  const cards = [];
+  const cards = []
   for (let i = 0; i <= 4; i++) {
     for (let j = 0; j < 4; j++) {
-      cards.push(i);
+      cards.push(i)
     }
   }
-  const shuffledCards = shuffle(cards);
+  const shuffledCards = shuffle(cards)
   return {
     cards: shuffledCards,
     penaltyPoints: 0,
     defenceHand: new Array<number>(3).fill(-1),
-  };
-};
+  }
+}
 
-export const turnStep = (callback: Function) => setTimeout(callback, 1000);
+export const turnStep = (callback: Function) => setTimeout(callback, 1000)
